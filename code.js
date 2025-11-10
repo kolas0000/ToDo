ecb0ed5e5b3aee859484dfc291e85daac7a6a5b3
kolas0000 <maksimovavika372@gmail.com>
2025-11-10 20:08:41 +0300

    six

--- code.js
+++ code.js
@@ -1,28 +1,34 @@
 const modal = document.getElementById('add');
 const wind = document.getElementById('modelWindow');
 const closeWind = document.getElementById('closeWindow');
+const backgroundLayer = document.getElementById('layer');
 
 modal.onclick = openModel;
 closeWind.onclick = closeWindow;
 
+
 function openModel() {
     wind.style.display = "flex";
+    backgroundLayer.style.display = "flex";
 }
 
 function closeWindow() {
     wind.style.display = "none";
+    backgroundLayer.style.display = "none";
 }
 
 let body = document.getElementsByTagName("body")[0];
 const geth1 = document.getElementById('geth1');
+let btnStyle = document.getElementById('stylebtn');
 
-
-document.getElementsByClassName("changeStyle")[0].addEventListener("click", () => {
+btnStyle.addEventListener("click", () => {
         body.classList.toggle("style-black");
         body.classList.toggle("style-white");
         wind.classList.toggle("model-Window-black");
         wind.classList.toggle("model-Window-white");
         geth1.classList.toggle("type-black");
         geth1.classList.toggle("type-white");
+        btnStyle.classList.toggle("change-Sun");
+        btnStyle.classList.toggle("change-Style");
 })
 
